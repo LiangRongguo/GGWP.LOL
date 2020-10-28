44e7b6b001d6cbb8f31fa2dce72d363b7b73cbab
LiangRongguo <l_rongguo@outlook.com>
2020-10-27 22:18:51 -0400

    initialized the avatar dropdown menu, enabling taking users to the summoner page and to sign out

--- src/components/summoner-avatar-dropdown/summoner-avatar-dropdown.component.jsx
+++ src/components/summoner-avatar-dropdown/summoner-avatar-dropdown.component.jsx
@@ -1,23 +1,80 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 import { setAvatarDropdownHidden } from "../../redux/avatar-dropdown/avatar-dropdown.actions";
+import { getSummonerAvatar } from "../../firebase/firebase.utils";
 
 import "./summoner-avatar-dropdown.styles.scss";
 
 import { auth } from "../../firebase/firebase.utils";
 
 const AvatarDropdown = ({ currentUser, setAvatarDropdownHidden }) => {
+  const [avatarUrl, setAvatarUrl] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchAvatarUrl = async () => {
+      setAvatarUrl(await getSummonerAvatar(currentUser));
+    };
+    fetchAvatarUrl();
+  }, [currentUser]);
+
   return (
     <div className="avatar-dropdown">
-      <div
-        className="option"
+      <Link
+        className="Profile"
+        to="/summoner"
         onClick={() => {
           setAvatarDropdownHidden();
-          auth.signOut();
         }}
       >
-        SIGN OUT
+        <Avatar
+          className="SummonerAvatar"
+          alt="Summoner Avatar"
+          src={avatarUrl}
+        />
+        <div className="SummonerName">
+          {currentUser.summoner_name == null || currentUser.summoner_name === ""
+            ? "Summoner"
+            : currentUser.summoner_name}
+        </div>
+      </Link>
+
+      <div className="divider">————————————</div>
+
+      <div className="Username">
+        <span className="row1">Signed in as</span>
+        <br></br>
+        <span className="row2">{currentUser.displayName}</span>
+      </div>
+
+      <div className="option-row" to="/summoner">
+        <AccountCircleIcon className="icon" color="action" />
+        <Link
+          className="long-option"
+          onClick={() => {
+            setAvatarDropdownHidden();
+          }}
+          to="/summoner"
+        >
+          Manage Summoner
+        </Link>
+      </div>
+
+      <div className="option-row">
+        <ExitToAppIcon className="icon" color="action" />
+        <div
+          className="option"
+          onClick={() => {
+            setAvatarDropdownHidden();
+            auth.signOut();
+          }}
+        >
+          Sign Out
+        </div>
       </div>
     </div>
   );
